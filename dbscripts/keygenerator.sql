6e0356dfefaca90f19df954c212d64ac20f4005e
Arunan <arunans23@gmail.com>
2021-03-28 01:29:07 +0530

    Add tenant jar to buildspec

--- dbscripts/keygenerator.sql
+++ dbscripts/keygenerator.sql
@@ -29,6 +29,17 @@ CREATE TABLE `TOKEN_INFO`
   AUTO_INCREMENT = 32
   DEFAULT CHARSET = latin1;
 
+  DROP TABLE IF EXISTS `TENANT_INFO`;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `TENANT_INFO`
+(
+    `tenant_id`     varchar(50) NOT NULL,
+    `tenant_name`   varchar(50) NOT NULL,
+    PRIMARY KEY (`tenant_id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 32
+  DEFAULT CHARSET = latin1;
 
 # todo add constraint
 #
