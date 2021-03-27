6b196d3faade4b15733ddd750fad39818dfea343
inthirakumaaran <inthira.14@cse.mrt.ac.lk>
2021-03-28 01:22:25 +0530

    Updated DBScript.

---
+++ dbscripts/TokenGenerator.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS `TOKEN_INFO`;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `TOKEN_INFO`
+(
+    `token`        varchar(1000) NOT NULL,
+    `tenant_id`    varchar(50)   NOT NULL,
+    `user_name`    varchar(50)   NOT NULL,
+    `app_id`       varchar(50)   NOT NULL,
+    `created_date` varchar(50)   NOT NULL,
+    `expiry_time`  varchar(50)   NOT NULL,
+    PRIMARY KEY (`token`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 32
+  DEFAULT CHARSET = latin1;
+
