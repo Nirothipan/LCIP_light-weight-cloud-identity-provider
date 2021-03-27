3a1de32d3730e83d374bea1b83143245bba97760
Nirothipan <nirothipan@gmail.com>
2021-03-27 18:33:02 +0530

    Parametrize form lambda

--- user-management/src/main/java/user/management/UserRegistrationLambda.java
+++ user-management/src/main/java/user/management/UserRegistrationLambda.java
@@ -7,10 +7,9 @@ import user.management.model.entity.UserDataEntity;
 
 public class UserRegistrationLambda extends UserManagement implements RequestHandler<UserDataEntity, Object> {
 
-
     @Override
     public Object handleRequest(UserDataEntity userData, Context context) {
-        return addUser();
+        return addUser(userData);
     }
 
     public static void main(String[] args) {
@@ -18,8 +17,26 @@ public class UserRegistrationLambda extends UserManagement implements RequestHan
         addUser();
     }
 
-    private static Object addUser(){
-        UserDataEntity  userData = new UserDataEntity ();
+    private static Object addUser(UserDataEntity data) {
+        UserDataEntity userData = new UserDataEntity();
+        userData.setId(data.getId());
+        userData.setUsername(data.getUsername());
+        userData.setUserEmail(data.getUserEmail());
+        userData.setPassword(data.getPassword());
+
+        JsonObject response = new JsonObject();
+        try {
+            response = userManager.addUser(userData);
+        } catch (Exception e) {
+            System.out.println("Exception :: " + e);
+            e.printStackTrace();
+        }
+        System.out.println("Data added :" + response.toString());
+        return response.toString();
+    }
+
+    private static Object addUser() {
+        UserDataEntity userData = new UserDataEntity();
         userData.setId("122323");
         userData.setUsername("niro12212");
         userData.setUserEmail("niro@wso2.com");
@@ -34,6 +51,5 @@ public class UserRegistrationLambda extends UserManagement implements RequestHan
         System.out.println("Data added :" + response.toString());
         return response;
     }
-
 
 }
