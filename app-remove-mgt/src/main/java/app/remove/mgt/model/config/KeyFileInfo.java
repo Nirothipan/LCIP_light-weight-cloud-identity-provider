d89aa8d03a47437e69a546227eaf57ac21fb4e74
inthirakumaaran <inthira.14@cse.mrt.ac.lk>
2021-03-27 17:30:01 +0530

    add remove application lambda

---
+++ app-remove-mgt/src/main/java/app/remove/mgt/model/config/KeyFileInfo.java
@@ -0,0 +1,15 @@
+package app.remove.mgt.model.config;
+
+public class KeyFileInfo {
+    private String location;
+
+    public String getLocation() {
+
+        return location;
+    }
+
+    public void setLocation(String location) {
+
+        this.location = location;
+    }
+}
---
+++ app-remove-mgt/src/main/java/app/remove/mgt/utils/Constants.java
@@ -0,0 +1,34 @@
+package app.remove.mgt.utils;
+
+/**
+ * This class includes the constance used in the licensekey.generator package.
+ *
+ * @since 1.0.0
+ */
+public class Constants {
+
+    private Constants() {
+
+    }
+
+    /**
+     * Configuration constants.
+     */
+    public static class Configurations {
+
+        public static final String CONFIGURATION_YAML = "licensekeygenerator.yaml";
+    }
+
+    /**
+     * Database constants.
+     */
+    public static class Database {
+
+        public static final String PERSISTENCE_UNIT_NAME = "user.management.model.UserData";
+        public static final String JDBC_URL = "javax.persistence.jdbc.url";
+        public static final String JDBC_USER = "javax.persistence.jdbc.user";
+        public static final String JDBC_PASSWORD = "javax.persistence.jdbc.password";
+        public static final String C3P0_MAX_CONNECTION_POOL_SIZE = "hibernate.c3p0.max_size";
+
+    }
+}
