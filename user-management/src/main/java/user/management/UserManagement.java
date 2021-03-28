ef388a708f39e486ffdddb8dd674b0d8cc78f08f
Nirothipan <nirothipan@gmail.com>
2021-03-28 14:06:52 +0530

    Add json

--- user-management/src/main/java/user/management/UserManagement.java
+++ user-management/src/main/java/user/management/UserManagement.java
@@ -56,4 +56,5 @@ public class UserManagement {
         return gson.toJson(json);
     }
 
+
 }
--- user-management/src/main/java/user/management/lambda/GetUserDetails.java
+++ user-management/src/main/java/user/management/lambda/GetUserDetails.java
@@ -1,28 +1,36 @@
 package user.management.lambda;
 
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.json.simple.JSONObject;
 import user.management.UserManagement;
 import user.management.model.entity.UserDataEntity;
 
-public class GetUserDetails extends UserManagement implements RequestHandler<UserDataEntity, UserDataEntity> {
+public class GetUserDetails extends UserManagement implements RequestHandler<UserDataEntity, Object> {
 
     @Override
-    public UserDataEntity handleRequest(UserDataEntity userDataEntity,
-                                        com.amazonaws.services.lambda.runtime.Context context) {
+    public Object handleRequest(UserDataEntity userDataEntity, com.amazonaws.services.lambda.runtime.Context context) {
         return getUser(userDataEntity.getUserName(), userDataEntity.getTenantId());
     }
 
     public static void main(String[] args) {
-        System.out.println(getUser("ram", "del"));
+        System.out.println(getUser("ram", "del").toString());
     }
 
-    private static UserDataEntity getUser(String name, String id) {
+    private static Object getUser(String name, String id) {
 
         try {
-            return userManager.getUser(name, id);
+            UserDataEntity userDataEntity = userManager.getUser(name, id);
+            if (userDataEntity == null) {
+                return null;
+            }
+            JSONObject user = new JSONObject();
+            user.put("Tenant Name", userDataEntity.getTenantId());
+            user.put("User Name", userDataEntity.getUserName());
+            user.put("Email", userDataEntity.getUserEmail());
+            return user;
         } catch (Exception e) {
             e.printStackTrace();
-            return new UserDataEntity(); //getErrorOutput(e.getMessage());
+            return getErrorOutput(e.getMessage());
         }
     }
 
